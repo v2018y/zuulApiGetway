f00839cf276a1537a75074b61994fb38fcc2f8a2
Vishvanath Surwashe <vsurwshe@gmail.com>
2019-07-27 14:10:11 +0530

    0.1.0 : Start The Zuul Api Getway

---
+++ src/main/java/com/vany/ZuulApiGatewayApplication.java
@@ -0,0 +1,30 @@
+package com.vany;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class ZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
+	
+	@Bean
+	 public UserDetailsService userDetailsService() {
+	  UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
+	   .build();
+	  return new InMemoryUserDetailsManager(user);
+	 }
+
+}
